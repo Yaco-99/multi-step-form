0347fa190a9194c03015d8d8b38cc19d7ecbabc8
Yaco-99 <coopman.yannick@gmail.com>
2022-05-27 14:36:32 +0200

    feat: install zustand

--- src/views/ContactFormViews/ContactBatmanView/ContactBatmanView.tsx
+++ src/views/ContactFormViews/ContactBatmanView/ContactBatmanView.tsx
@@ -1,20 +1,31 @@
-import { useState } from "react";
+import { useRouter } from "next/router";
+import { useEffect, useState } from "react";
 import AddressStep from "./Steps/AddressStep/AddressStep";
 import InformationStep from "./Steps/InformationStep/InformationStep";
 import { ContactBatmanViewProps } from "./types";
 
 const ContactBatmanView = ({}: ContactBatmanViewProps): JSX.Element => {
+  const router = useRouter();
+
   const [step, setStep] = useState(0);
+
+  const toNextStep = () => {
+    setStep((current) => current + 1);
+
+    router.push(
+      { pathname: router.pathname, query: { ...router.query, step: step + 1 } },
+      undefined,
+      {
+        shallow: true,
+      }
+    );
+  };
 
   switch (step) {
     case 0:
-      return (
-        <AddressStep submitFnc={() => setStep((current) => current + 1)} />
-      );
+      return <InformationStep submitFnc={toNextStep} />;
     case 1:
-      return (
-        <InformationStep submitFnc={() => setStep((current) => current + 1)} />
-      );
+      return <AddressStep submitFnc={toNextStep} />;
     default:
       throw new Error(`step ${step} does not exist`);
   }
